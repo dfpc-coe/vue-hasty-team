e1cbc86c64b4e63032bcd5b9cd21b950f0758bf9
ingalls <nick@ingalls.ca>
2025-07-11 10:14:53 -0700

    Add Cursor Move

--- demo/src/Block.vue
+++ demo/src/Block.vue
@@ -1,28 +1,41 @@
 <template>
     <div
-        class='blockelem noselect'
-        :class='{
-            "blockdisabled": props.disabled,
+        class='card cursor-move'
+        :style='{
+            "background-color: var(--tblr-gray-300);": props.disabled,
         }'
         :draggable='props.draggable'
     >
-        <div class='grabme'>
-            <img src='/assets/grabme.svg'>
-        </div>
-        <div class='blockin'>
-            <div class='blockico'>
-                <span />
-                <img :src='props.image'>
+        <div class='card-header'>
+            <IconGripVertical
+                v-if='props.draggable'
+                :size='24'
+                stroke='1'
+                class='me-2'
+                color='gray'
+            />
+            <span class='card-title' v-text='props.title'/>
+
+            <div class='ms-auto'>
+                <img
+                    width='32'
+                    :src='props.image'
+                >
             </div>
-            <div class='blocktext'>
-                <p class='blocktitle' v-text='props.title'/>
-                <p class='blockdesc' v-text='props.description'/>
+        </div>
+        <div class='card-body'>
+            <div>
+                <p v-text='props.description'/>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
+import {
+    IconGripVertical
+} from '@tabler/icons-vue'
+
 const props = defineProps({
     image: {
         type: String,
