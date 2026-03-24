0a5a18af1a759b82e7e7cccc1c24350e8663ef73
ingalls <nick@ingalls.ca>
2026-03-24 16:59:11 -0600

    Add GH Pages

---
+++ demo/src/TablerIconButton.vue
@@ -0,0 +1,9 @@
+<template>
+    <button
+        type='button'
+        class='btn btn-icon btn-sm btn-ghost-secondary'
+        v-bind='$attrs'
+    >
+        <slot />
+    </button>
+</template>
---
+++ demo/src/TablerInput.vue
@@ -0,0 +1,21 @@
+<template>
+    <input
+        class='form-control form-control-sm'
+        v-bind='$attrs'
+        :value='modelValue'
+        @input='emit("update:modelValue", $event.target.value)'
+    >
+</template>
+
+<script setup>
+defineOptions({ inheritAttrs: false });
+
+defineProps({
+    modelValue: {
+        type: String,
+        default: ''
+    }
+});
+
+const emit = defineEmits(['update:modelValue']);
+</script>
